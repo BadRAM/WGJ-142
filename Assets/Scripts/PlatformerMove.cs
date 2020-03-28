b5ec251c338c21cd73c4832b90312f51da10f4c5
BadRAM <dgamebillda@gmail.com>
2020-03-28 15:52:08 -0700

    Branched & added gitignore

---
+++ Assets/Scripts/PlatformerMove.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerMove : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    
+    private Rigidbody2D _rigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+    
+    void FixedUpdate()
+    {
+        _rigidbody.AddForce(Vector2.left * Time.deltaTime * Input.GetAxis("Horizontal") * speed);
+    }
+}
